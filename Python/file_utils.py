15b47e2dc4f03936f230569902fcc636be2da21e
Charlotte <conte.charlotte56@laposte.net>
2025-01-13 16:33:14 +0100

    Rectification cas de test

--- Python/file_utils.py
+++ Python/file_utils.py
@@ -4,11 +4,10 @@ def diff(file_first, file_second):
     """Fonction retournant True si deux fichiers sont différents."""
     with open (file_first, encoding="utf-8") as fileid1:
         with open (file_second, encoding="utf-8") as fileid2:
-            return fileid1.read() != fileid2.read() 
-    
+            return fileid1.read() != fileid2.read()
 
 def same(file_first, file_second):
     """Fonction retournant True si deux fichiers sont pareils."""
     with open (file_first, encoding="utf-8") as fileid1:
         with open (file_second, encoding="utf-8") as fileid2:
-            return fileid1.read() == fileid2.read() 
+            return fileid1.read() == fileid2.read()
