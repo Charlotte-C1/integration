c786a57b95f3d4cf8e5b530cc23b8bb20a5130e2
Charlotte <conte.charlotte56@laposte.net>
2025-01-13 16:30:17 +0100

    Rectification cas de test same (python)

--- Python/file_utils.py
+++ Python/file_utils.py
@@ -1,12 +1,14 @@
 """Module manipulant des fichiers texte."""
 
 def diff(file_first, file_second):
+    """Fonction retournant True si deux fichiers sont différents."""
     with open (file_first) as fileid1:
         with open (file_second) as fileid2:
             return fileid1.read() != fileid2.read() 
     
 
 def same(file_first, file_second):
+    """Fonction retournant True si deux fichiers sont pareils."""
     with open (file_first) as fileid1:
         with open (file_second) as fileid2:
             return fileid1.read() == fileid2.read() 
