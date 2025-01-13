e094fbd09c32ceee027ed21bc1dd76dd06705616
Charlotte <conte.charlotte56@laposte.net>
2025-01-13 15:42:29 +0100

    Modif 1 Fonction diff (python)

--- Python/file_utils.py
+++ Python/file_utils.py
@@ -1,9 +1,9 @@
 """Module manipulant des fichiers texte."""
 
 def diff(file_first, file_second):
-    with open (file_first) as file_id1:
-		with open (file_second) as file_id2:
-			return file_id1.read() != file_id2.read() 
+    with open (file_first) as fileid1:
+		with open (file_second) as fileid2:
+			return fileid1.read() != fileid2.read() 
     
 
 def same(file_first, file_second):
